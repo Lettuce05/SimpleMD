685737ee5370b918b35ee2045878fe60023aeaa6
Anthony Romaine <anthony.romaine1@gmail.com>
2022-12-22 00:38:18 +0000

    Add split and preview functionality

--- header.js
+++ header.js
@@ -23,8 +23,51 @@ export function createHeader() {
 
   // create right side of header
   const rightDiv = createElement("div", { classes: ["header-right"] });
+  const splitBtn = createElement("button", { id: "splitBtn", classes: ["header-btn"] });
+  splitBtn.innerText = "Split";
+  splitBtn.onclick = function() {
+    if (document.getElementById("previewContainer") && document.getElementById("editor")) {
+      let preview = document.getElementById("previewContainer");
+      let editor = document.getElementById("editor");
+
+      if (preview.style.display === "none" || (preview.style.display === "block" && editor.style.display === "none")) {
+        preview.style.display = "block";
+        preview.style.width = "50%";
+        editor.style.display = "block";
+        editor.style.width = "50%";
+      } else {
+        preview.style.display = "none";
+        editor.style.display = "block";
+        editor.style.width = "100%";
+      }
+    }
+  }
+  rightDiv.appendChild(splitBtn);
+
+  // create preview button
+  const previewBtn = createElement("button", { id: "previewBtn", classes: ["header-btn"] });
+  previewBtn.innerText = "Preview";
+  previewBtn.onclick = function() {
+    if (document.getElementById("previewContainer") && document.getElementById("editor")) {
+      let preview = document.getElementById("previewContainer");
+      let editor = document.getElementById("editor");
+
+      if (preview.style.display !== "none" && editor.style.display !== "none") {
+        return;
+      } else if (editor.style.display === "none") {
+        preview.style.display = "none";
+        editor.style.display = "block";
+        editor.style.width = "100%";
+      } else {
+        editor.style.display = "none";
+        preview.style.display = "block";
+        preview.style.width = "100%";
+      }
+    }
+  }
+  rightDiv.appendChild(previewBtn);
   // create saveBtn
-  const saveBtn = createElement("button", { id: "saveBtn" });
+  const saveBtn = createElement("button", { id: "saveBtn", classes: ["header-btn"] });
   saveBtn.innerText = "Save";
   saveBtn.onclick = function() {
     let modal = document.getElementById("saveModal");
